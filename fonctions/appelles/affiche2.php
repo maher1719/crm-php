c4431e667ce81282aea96eab0914fe5c8b28073a
maher <ba.maher94@gmail.com>
2020-01-23 03:47:33 +0100

    test comment3

--- fonctions/appelles/affiche2.php
+++ fonctions/appelles/affiche2.php
@@ -6,7 +6,7 @@ $id=$_SESSION['id'];
 $appelles=new Appelle();
 $select_appel="";
 //just comment is good
-//comment 23
+//comment 23334
 if(isset($_GET["id_appel"]))
 {
 	$select_appel="where appel.id=".$_GET["id_appel"];
